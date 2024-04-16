400b64aa82a924710587232113faab7560e95333
imran <imran.h.ovi.95@gmail.com>
2024-04-16 17:30:25 +0600

    production -

--- client/src/app/api/auth/[...nextauth]/route.ts
+++ client/src/app/api/auth/[...nextauth]/route.ts
@@ -46,7 +46,6 @@ const authOptions: any = {
   },
 };
 
-const handler = NextAuth(authOptions);
+export const handler = NextAuth(authOptions);
 
-export { handler as get, handler as POST };
-
+export { handler as GET, handler as POST };
