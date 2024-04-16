726aa927e3b4c0fcfac44f831bbc280c62d21789
imran <imran.h.ovi.95@gmail.com>
2024-04-16 18:03:06 +0600

    production -

---
+++ client/src/lib/auth.ts
@@ -0,0 +1,46 @@
+import UserModel from "@/models/user";
+import { connect } from "@/utils/db";
+import bcrypt from "bcrypt";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions: any = {
+  providers: [
+    CredentialsProvider({
+      credentials: {
+        email: {},
+        password: {},
+      },
+      async authorize(credentials, req) {
+        await connect();
+        const { email, password }: any = credentials;
+        try {
+          const user = await UserModel.findOne({ email });
+          if (!!user) {
+            const matchPassword = await bcrypt.compare(
+              password,
+              user?.password
+            );
+            if (matchPassword) {
+              return user;
+            }
+          }
+          return null;
+        } catch (error) {
+          throw new Error("user not found");
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account }: any) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token }: any) {
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
+};
