3c42d8cdd24d26457cb3bab02e4b9038c7fed81b
imran <imran.h.ovi.95@gmail.com>
2024-04-23 14:54:33 +0600

    URLSearchParams boundary issue solved

--- client/src/app/(client)/payment/success/page.tsx
+++ client/src/app/(client)/payment/success/page.tsx
@@ -1,52 +1,50 @@
 "use client";
 
 import React, { useEffect } from "react";
-import { useSearchParams } from "next/navigation";
-import { useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
 import { useCart } from "@/utils/cartContext";
 
 const PaymentSuccessPage = () => {
-  const searchParams = useSearchParams();
-  const trxId = searchParams.get("id");
-  const router = useRouter();
   const { data: session } = useSession();
   const { cart, removeFromCart } = useCart();
 
-  const fetchPaymentData = async () => {
-    const gw = localStorage.getItem("gw");
-    console.log(gw);
-    try {
-      const response = await fetch(`/api/database/payment`, {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ trxId: trxId, status: true, gw: gw }),
-      });
-
-      if (response.ok) {
-        console.log("Payment status updated successfully");
-        localStorage.removeItem("gw");
-        cart.forEach((item) => {
-          removeFromCart(item.product.id);
-        });
-        setTimeout(() => {
-          router.push("/dashboard/user-payment");
-        }, 3000);
-      } else {
-        console.error("Failed to update payment status");
-      }
-    } catch (error) {
-      console.error("Error updating payment status:", error);
-    }
-  };
-
   useEffect(() => {
+    const fetchPaymentData = async () => {
+      const searchParams = new URLSearchParams(window.location.search);
+      const trxId = searchParams.get("id");
+      const gw = localStorage.getItem("gw");
+
+      try {
+        const response = await fetch(`/api/database/payment`, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ trxId: trxId, status: true, gw: gw }),
+        });
+
+        if (response.ok) {
+          console.log("Payment status updated successfully");
+          localStorage.removeItem("gw");
+          cart.forEach((item) => {
+            removeFromCart(item.product.id);
+          });
+          setTimeout(() => {
+            window.location.href = "/dashboard/user-payment";
+          }, 3000);
+        } else {
+          console.error("Failed to update payment status");
+        }
+      } catch (error) {
+        console.error("Error updating payment status:", error);
+      }
+    };
+
+    const trxId = new URLSearchParams(window.location.search).get("id");
     if (trxId) {
       fetchPaymentData();
     }
-  }, [trxId]);
+  }, []);
 
   return (
     <div className="container">
